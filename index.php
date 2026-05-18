aa23e496d17b543498597dd0395a5cc1cfb02385
Sceletonko <galaxi2901@gmail.com>
2026-05-18 14:38:53 +0200

    new start...

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Projekt bol resetovaný</h1>";
+echo "<p>Laravel bol odstránený. Toto je základná index.php stránka.</p>";
+echo "<p>Aktuálny čas: " . date('Y-m-d H:i:s') . "</p>";
+echo "<p>Pushovanie na GitHub by malo fungovať.</p>";
